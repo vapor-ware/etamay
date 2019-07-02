718e4d239f509cc347fe6cdf4d9c852043f352fa
Marco Ceppi <marco@ceppi.net>
2019-07-02 06:39:44 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# etaMay - Metadata Service
---
+++ etamay/__init__.py
@@ -0,0 +1,10 @@
+"""etaMay Metadata Service"""
+
+
+__title__ = 'etamay'
+__version__ = '0.0.1'
+__description__ = 'etaMay Metadata'
+__author__ = 'Marco Ceppi'
+__author_email__ = 'marco@vapor.io'
+__url__ = 'https://github.com/vapor-ware/etamay'
+__license__ = 'GNU General Public License v3.0'
